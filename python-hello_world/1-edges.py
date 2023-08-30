4e2c3ecb2246432f6f16a737b6bb3e31db8e3f44
Olliehub <charlibossubom@gmail.com>
2023-08-30 10:36:26 +0100

    cut and paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+word = "Holberton"
+# print the letters of the variable word
+
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on"))
+print("Middle word: {}".format("oberto"))
