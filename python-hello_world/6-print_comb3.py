e1b27ac95312b0cd34fa90eda02a0b1e6dc2af0d
Olliehub <charlibossubom@gmail.com>
2023-08-31 20:47:53 +0100

    task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-# Write a program that prints all possible different combinations of two digits.
+# Write a program that prints all possible different combinations of two digits
 
 def print_two_digits():
   for i in range(10):
     for j in range(i + 1, 10):
       if i != j:
         print(", {:02d}".format(10 * i + j), end="")
+
 print_two_digits()
