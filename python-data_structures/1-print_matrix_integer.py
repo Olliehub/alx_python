9d980092a631680e3f119937e90fde6951d37ba7
Olliehub <charlibossubom@gmail.com>
2023-10-05 02:06:32 +0100

    Lists. Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, num in enumerate(row):
-            if i < len(row) - 1:
-                print("{:d}".format(num), end="\t")
-            else:
-                print("{:d}".format(num), end="")
+    if not matrix:
+        return None
+
+    for submatrix in matrix:
+        for i in range(len(submatrix)):
+            print("{:d}".format(submatrix[i]), end="")
+            if i < len(submatrix) - 1:
+                print(" ", end="")
         print()
 
 matrix = [[1,2,3], [4,5,6], [7,8,9]]
