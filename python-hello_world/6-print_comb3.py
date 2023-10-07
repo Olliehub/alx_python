be2303bc974c49e5c2d893b4ff56c3fd543e002f
Olliehub <charlibossubom@gmail.com>
2023-10-07 14:00:22 +0100

    Program that prints possible different combinations

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-# Write a program that prints all possible different combinations of two digits
+# Program that prints all possible different combinations of two digits
 
-def print_two_digits():
-  for i in range(10):
+for i in range(10):
     for j in range(i + 1, 10):
-      if i != j:
-        print(", {:02d}".format(10 * i + j), end="")
-
-print_two_digits()
+        print("{:d}{:d}".format(i, j), end=", " if i < 8 else "\n")
